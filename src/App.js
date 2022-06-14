a1c26aa83b4f70d40499f625e4f9c1919cc6af38
Omar Maroki <omar987@hotmail.co.uk>
2022-06-14 15:09:30 +0100

    fetch data and start to fill MUI table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import MyTable from './components/MyTable';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<MyTable />
+		</div>
+	);
 }
 
 export default App;
